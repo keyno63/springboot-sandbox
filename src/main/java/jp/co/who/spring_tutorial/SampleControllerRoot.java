0386e9998c45c60679391c693fa82da2e36ad5d6
Keyno <kofujiw@gmail.com>
2019-04-21 15:43:57 +0900

    add web containor

--- src/main/java/jp/co/who/spring_tutorial/SampleControllerRoot.java
+++ src/main/java/jp/co/who/spring_tutorial/SampleControllerRoot.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleControllerRoot {
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    //@RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @RequestMapping(value="/hello")
     public String hello() {
         return "hello";
     }
