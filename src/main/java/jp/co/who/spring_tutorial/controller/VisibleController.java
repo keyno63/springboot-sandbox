072c78b4828bede14b62e14898dbceb230ad71f8
Keyno <kofujiw@gmail.com>
2020-08-21 03:03:22 +0900

    なんか足してみた

--- src/main/java/jp/co/who/spring_tutorial/controller/VisibleController.java
+++ src/main/java/jp/co/who/spring_tutorial/controller/VisibleController.java
@@ -2,6 +2,13 @@ package jp.co.who.spring_tutorial.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Controller
 public class VisibleController {
@@ -14,5 +21,17 @@ public class VisibleController {
     @GetMapping("/index")
     public  String home(){
         return "sample/home";
+    }
+
+    @RequestMapping(value="/search")
+    public ModelAndView search(
+            @RequestParam("key") String key, ModelAndView mv) {
+        mv.setViewName("search");
+        //List<OfuroEntity> list = repository.findByNameLike("%" + key + "%");
+        //List<String> list = List.of("neo");
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        mv.addObject("list", list);
+        return mv;
     }
 }
