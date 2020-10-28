e220a90a19536b096c4c29404745571f7b4a7562
Keyno <kofujiw@gmail.com>
2020-10-29 02:30:43 +0900

    impliment header controller.

---
+++ src/main/java/jp/co/who/spring_tutorial/controller/HeaderController.java
@@ -0,0 +1,33 @@
+package jp.co.who.spring_tutorial.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Header を制御する処理をまとめた Controller
+ */
+@RequestMapping("/header")
+@Controller
+public class HeaderController {
+
+    @GetMapping("/userAgent")
+    @ResponseBody
+    public String userAgent(HttpServletRequest request) {
+        return request.getHeader(HeaderValues.USER_AGENT);
+    }
+
+    @GetMapping("/cookie")
+    @ResponseBody
+    public String cookie(HttpServletRequest request) {
+        return request.getHeader(HeaderValues.COOKIE);
+    }
+
+    private static class HeaderValues {
+        private static String USER_AGENT = "User-Agent";
+        private static String COOKIE = "Cookie";
+    }
+}
