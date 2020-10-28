ff27fe2aaebd0c49036a735e6420b37d2451d9e3
Keyno <kofujiw@gmail.com>
2020-10-29 03:38:46 +0900

    header controller Test.

---
+++ src/test/java/jp/co/who/spring_tutorial/controller/HeaderControllerTest.java
@@ -0,0 +1,35 @@
+package jp.co.who.spring_tutorial.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class HeaderControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setupMockMvc() {
+        this.mockMvc = MockMvcBuilders
+                .standaloneSetup(new HeaderController())
+                .build();
+    }
+
+    @Test
+    public void getCookieEndpoint() {
+        mockMvc.perform(get("/header/cookie"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getUserAgentEndpoint() {
+        mockMvc.perform(get("/header/userAgent"))
+                .andExpect(status().isOk());
+    }
+}
