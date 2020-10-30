e61d5497af67f79c90250d7dd0a8d8c62ef08e3f
Keyno <kofujiw@gmail.com>
2020-10-30 23:57:30 +0900

    header controller Test. perform で Exception するように変更！

--- src/main/java/jp/co/who/spring_tutorial/controller/HeaderController.java
+++ src/main/java/jp/co/who/spring_tutorial/controller/HeaderController.java
@@ -2,10 +2,9 @@ package jp.co.who.spring_tutorial.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * Header を制御する処理をまとめた Controller
@@ -16,14 +15,14 @@ public class HeaderController {
 
     @GetMapping("/userAgent")
     @ResponseBody
-    public String userAgent(HttpServletRequest request) {
-        return request.getHeader(HeaderValues.USER_AGENT);
+    public String userAgent(@RequestHeader(HeaderValues.USER_AGENT) String value) {
+        return value;
     }
 
     @GetMapping("/cookie")
     @ResponseBody
-    public String cookie(HttpServletRequest request) {
-        return request.getHeader(HeaderValues.COOKIE);
+    public String cookie(@RequestHeader(HeaderValues.COOKIE) String value) {
+        return value;
     }
 
     private static class HeaderValues {
