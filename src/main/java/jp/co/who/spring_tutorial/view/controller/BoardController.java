2b11334a420509257283839dd9413e05536c3c56
Keyno <kofujiw@gmail.com>
2021-01-01 00:03:25 +0900

    add board controller

--- src/main/java/jp/co/who/spring_tutorial/view/controller/BoardController.java
+++ src/main/java/jp/co/who/spring_tutorial/view/controller/BoardController.java
@@ -1,4 +1,15 @@
 package jp.co.who.spring_tutorial.view.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
 public class BoardController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "index";
+    }
 }
