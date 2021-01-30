c698eed6b883edba03b377b614e92a6a6d71e459
Keyno <kofujiw@gmail.com>
2021-01-30 22:12:45 +0900

    user テーブル追加

--- src/main/resources/database/data.sql
+++ src/main/resources/database/data.sql
@@ -1,3 +1,22 @@
-INSERT INTO book (id, book_category_id, `name`) VALUES (1, 3, 'rashomon'), (2, 1, 'blue beard'), (3, 2, 'it');
+INSERT INTO
+    book
+    (id, book_category_id, `name`)
+VALUES
+   (1, 3, 'rashomon'),
+   (2, 1, 'blue beard'),
+   (3, 2, 'it');
 
-INSERT INTO book_category (id, `name`) VALUES (1, 'foreign fairy tale'), (2, 'foreign horror'), (3, 'novel');
+INSERT INTO
+    book_category (id, `name`)
+VALUES
+   (1, 'foreign fairy tale'),
+   (2, 'foreign horror'),
+   (3, 'novel');
+
+INSERT INTO
+    `user` (id, `password`, `first_name`, `last_name`)
+VALUES
+    ('user1', '1234', 'taro', 'yamada'),
+    ('user2', '5678', 'hanako', 'yamada'),
+    ('user3', 'pass', 'ichiro', 'suzuki'),
+    ('user4', 'Pass', 'sakura', 'tanaka')
--- src/main/resources/database/schema.sql
+++ src/main/resources/database/schema.sql
@@ -1,3 +1,6 @@
 CREATE TABLE IF NOT EXISTS book ( id INT NOT NULL, book_category_id INT NOT NULL, `name` text NOT NULL)
 
 CREATE TABLE IF NOT EXISTS book_category (id INT NOT NULL, `name` text NOT NULL)
+
+DROP TABLE IF EXISTS `user`
+CREATE TABLE `user` (id text NOT NULL, `password` text NOT NULL, `first_name` text NOT NULL, `last_name` text NOT NULL)
