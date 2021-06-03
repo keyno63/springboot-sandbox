118443f8165e49ffb0685e212914cd4a7f238371
Keyno <kofujiw@gmail.com>
2021-06-04 01:24:59 +0900

    過去のテストの一部を Junit5 化したかった

--- src/test/java/jp/co/who/spring_tutorial/service/MessageServiceIntegrationTest.java
+++ src/test/java/jp/co/who/spring_tutorial/service/MessageServiceIntegrationTest.java
@@ -1,33 +1,48 @@
 package jp.co.who.spring_tutorial.service;
 
+import jp.co.who.spring_tutorial.api.message.repository.MessageRepository;
 import jp.co.who.spring_tutorial.api.message.service.MessageService;
-import jp.co.who.spring_tutorial.config.MessageConfig;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.MessageSource;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = MessageConfig.class)
+@ExtendWith(MockitoExtension.class)
 public class MessageServiceIntegrationTest {
+
+    @Mock
+    MessageSource messageSource;
+
+    @Mock
+    MessageRepository messageRepository;
 
     @InjectMocks
     MessageService service;
 
-    //@Test
+    @BeforeEach
+    public void setUp() {
+        doReturn("Hello!!")
+                .when(messageSource)
+                .getMessage(anyString(), any(), any());
+    }
+
+    @Test
     public void メッセージバイコードの結合テスト() {
         System.out.println("start integration.");
         String actual = service.getMessageByCode("greeting");
-        assertThat(actual, is("Hello!!"));
+        assertThat(actual).isEqualTo( "Hello!!");
     }
 
     @Test
     public void hoge() {
-        return;
+        assertThat(true).isTrue();
     }
 }
