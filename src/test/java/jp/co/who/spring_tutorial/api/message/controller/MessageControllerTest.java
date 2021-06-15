fae4373343848678190da9a334aa940f81e4de9e
Keyno <kofujiw@gmail.com>
2021-06-16 06:08:57 +0900

    古いテストを少し直した

--- src/test/java/jp/co/who/spring_tutorial/api/message/controller/MessageControllerTest.java
+++ src/test/java/jp/co/who/spring_tutorial/api/message/controller/MessageControllerTest.java
@@ -1,28 +1,21 @@
 package jp.co.who.spring_tutorial.api.message.controller;
 
 import jp.co.who.spring_tutorial.api.message.service.MessageService;
-import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.doReturn;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@WebMvcTest(MessageController.class)
-//@ExtendWith(SpringExtension.class)
-@SpringBootTest
-@RunWith(SpringRunner.class)
+@WebMvcTest(MessageController.class)
 @AutoConfigureMockMvc
-public class MessageControllerTest {
+class MessageControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -31,8 +24,12 @@ public class MessageControllerTest {
     MessageService messageService;
 
     @Test
-    public void コントローラーのテスト() throws Exception {
+    void コントローラーのテスト() throws Exception {
+        doReturn("some")
+                .when(messageService)
+                .get();
         mockMvc.perform(get("/api/message/get"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().string("some"));
     }
 }
