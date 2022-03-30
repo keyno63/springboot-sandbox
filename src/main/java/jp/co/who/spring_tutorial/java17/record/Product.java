3734c4b801035e3ae006fe39aa3122337e065687
Keyno <kofujiw@gmail.com>
2022-03-30 23:16:42 +0900

    fix product

--- src/main/java/jp/co/who/spring_tutorial/java17/record/Product.java
+++ src/main/java/jp/co/who/spring_tutorial/java17/record/Product.java
@@ -1,15 +1,16 @@
 package jp.co.who.spring_tutorial.java17.record;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-//@EqualsAndHashCode
-@RequiredArgsConstructor
 public class Product {
     private final String id;
     private final String name;
     private final int value;
+
+    public Product(String id, String name, int value) {
+        this.id = id;
+        this.name = name;
+        this.value = value;
+    }
 }
