f9f08ffdeeb3c48ddd9185de8a1fcde255c562f1
Keyno <kofujiw@gmail.com>
2022-04-29 23:45:47 +0900

    optimize import

--- src/main/java/jp/co/who/spring_tutorial/view/controller/EchoErrorPageController.java
+++ src/main/java/jp/co/who/spring_tutorial/view/controller/EchoErrorPageController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
 import java.util.Objects;
 
 @RestController
